b56986ef09c31e9acf6ef6d16aeac8958417ba46
mariagarcia05 <161719459+mariagarcia05@users.noreply.github.com>
2025-02-23 22:13:10 +0100

    Delete AppTiendaOnlinne directory

--- AppTiendaOnlinne/Respository/PedidoRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.spring.AppTiendaOnlinne.Respository;
-
-import org.example.spring.AppTiendaOnlinne.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface PedidoRepository extends JpaRepository<Pedido,Integer> {
-}
